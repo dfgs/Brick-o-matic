b58717681cca78b74e60d34b7eca671a7f4780ac
dfgs <dfgs83@gmail.com>
2021-11-01 15:25:12 +0100

    Added color

---
+++ Brick-o-matic.Math.UnitTest/ColorUnitTest.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Brick_o_matic.Math.UnitTest
+{
+	[TestClass]
+	public class ColorUnitTest
+	{
+		[TestMethod]
+		public void ShouldInstanciate()
+		{
+			Color v;
+
+			v = new Color(0, 0, 0);
+			Assert.IsNotNull( v);
+			Assert.AreEqual(0, v.R);
+			Assert.AreEqual(0, v.G);
+			Assert.AreEqual(0, v.B);
+
+			v = new Color(1, 2, 3);
+			Assert.IsNotNull(v);
+			Assert.AreEqual(1, v.R);
+			Assert.AreEqual(2, v.G);
+			Assert.AreEqual(3, v.B);
+		}
+
+		[TestMethod]
+		public void ShouldEquals()
+		{
+
+			Assert.IsTrue(new Color(1, 2, 3).Equals((object)new Color(1, 2, 3)));
+			Assert.IsFalse(new Color(1, 2, 3).Equals((object)new Color(0, 2, 3)));
+			Assert.IsFalse(new Color(1, 2, 3).Equals((object)new Color(1, 0, 3)));
+			Assert.IsFalse(new Color(1, 2, 3).Equals((object)new Color(1, 2, 0)));
+		}
+
+		[TestMethod]
+		public void ShouldEqualsWithIEquatable()
+		{
+
+			Assert.IsTrue(new Color(1, 2, 3).Equals(new Color(1, 2, 3)));
+			Assert.IsFalse(new Color(1, 2, 3).Equals(new Color(0, 2, 3)));
+			Assert.IsFalse(new Color(1, 2, 3).Equals(new Color(1, 0, 3)));
+			Assert.IsFalse(new Color(1, 2, 3).Equals(new Color(1, 2, 0)));
+		}
+
+
+		[TestMethod]
+		public void ShouldEqualsWithOperator()
+		{
+			Assert.IsTrue(new Color(1, 2, 3)==new Color(1, 2, 3));
+			Assert.IsFalse(new Color(1, 2, 3) == new Color(0, 2, 3));
+			Assert.IsFalse(new Color(1, 2, 3) == new Color(1, 0, 3));
+			Assert.IsFalse(new Color(1, 2, 3) == new Color(1, 2, 0));
+		}
+		[TestMethod]
+		public void ShouldNotEqualsWithOperator()
+		{
+			Assert.IsFalse(new Color(1, 2, 3) != new Color(1, 2, 3));
+			Assert.IsTrue(new Color(1, 2, 3) != new Color(0, 2, 3));
+			Assert.IsTrue(new Color(1, 2, 3) != new Color(1, 0, 3));
+			Assert.IsTrue(new Color(1, 2, 3) != new Color(1, 2, 0));
+		}
+
+	
+
+
+
+	}
+}
---
+++ Brick-o-matic.Math/Color.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brick_o_matic.Math
+{
+	public struct Color:IEquatable<Color>
+	{
+		public byte R
+		{
+			get;
+			private set;
+		}
+		public byte G
+		{
+			get;
+			private set;
+		}
+		public byte B
+		{
+			get;
+			private set;
+		}
+		
+		public Color(byte R, byte G, byte B)
+		{
+			this.R = R;this.G = G;this.B = B;
+		}
+
+		public override string ToString()
+		{
+			return $"({R},{G},{B})";
+		}
+		public override bool Equals(object obj)
+		{
+			if (obj is Color otherColor) return ((otherColor.R == R) && (otherColor.G == G) && (otherColor.B == B));
+			return false;
+		}
+		public bool Equals(Color OtherColor)
+		{
+			return ((OtherColor.R == R) && (OtherColor.G == G) && (OtherColor.B == B));
+		}
+		public override int GetHashCode()
+		{
+			return ToString().GetHashCode();
+		}
+		public static bool operator ==(Color Color1, Color Color2)
+		{
+			return ((Color1.R == Color2.R) && (Color1.G == Color2.G) && (Color1.B == Color2.B));
+		}
+		public static bool operator !=(Color Color1, Color Color2)
+		{
+			return ((Color1.R != Color2.R) || (Color1.G != Color2.G) || (Color1.B != Color2.B));
+		}
+
+		
+
+	}
+}
